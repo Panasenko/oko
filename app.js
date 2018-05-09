d5475a587cc9460e4feca6cee72bd3be086e85e2
Stan <panasenkomaksim@gmail.com>
2018-05-09 23:24:57 +0300

    Откат изменений

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', routes);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
