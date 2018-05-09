c288820c7938bf7ad1090ed7292ceb0c4c845174
Stan <panasenkomaksim@gmail.com>
2018-05-09 18:05:15 +0300

    Добавлена заглушка к контроллеру АПИ

--- app_api/controllers/monitoring.js
+++ app_api/controllers/monitoring.js
@@ -1,34 +1,12 @@
 var mongoose = require('mongoose');
 var Mon = mongoose.model('Monitoring');
 
+
 var sendJSONresponse = function(res, status, content) {
     res.status(status);
-    res.json({});
+    res.json(content);
 };
 
-module.exports.getzabbix = function(req, res) {
-    console.log('Finding location details', req.params);
-    if (req.params) {
-        Mon
-            .find()
-            .exec(function(err, location) {
-                if (!location) {
-                    sendJSONresponse(res, 404, {
-                        "message": "locationid not found"
-                    });
-                    return;
-                } else if (err) {
-                    console.log(err);
-                    sendJSONresponse(res, 404, err);
-                    return;
-                }
-                console.log(location);
-                sendJSONresponse(res, 200, {});
-            });
-    } else {
-        console.log('No locationid specified');
-        sendJSONresponse(res, 404, {
-            "message": "No locationid in request"
-        });
-    }
+module.exports.getZabbix = function(req, res) {
+    sendJSONresponse(res, 200, {"status": "succsess"})
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -3,6 +3,6 @@ var router = express.Router();
 var CtrlMon = require('../controllers/monitoring');
 
 /* GET home page. */
-router.get('/', CtrlMon.getzabbix);
+router.get('/', CtrlMon.getZabbix);
 
 module.exports = router;
